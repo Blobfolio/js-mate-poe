98fbca582a89546fb2e07196a95e576ec18963e6
Josh Stoik <josh@blobfolio.com>
2019-09-29 19:15:18 -0700

    store data as blobs

--- src/js/_helpers.mjs
+++ src/js/_helpers.mjs
@@ -53,6 +53,55 @@ export const yDirection = function(movement) {
 // ---------------------------------------------------------------------
 
 /**
+ * Base64 to Blob
+ *
+ * It's weird. Binary data stored as base64-encoded strings takes up
+ * much less disk space than the same data stored in a TypedArray, but
+ * during runtime, data-URIs actually require more memory.
+ *
+ * Because our file values are constants, we can enjoy the best of both
+ * worlds by programmatically converting base64 to Blob once at startup.
+ *
+ * @param {string} data Data.
+ * @param {string} type Content type.
+ * @return {!Blob} Blob.
+ */
+export const base64toBlob = function(data, type) {
+	/** @const {string} */
+	const bytes = atob(data);
+
+	/** @type {number} */
+	let length = bytes.length;
+
+	/** @type {Uint8Array} */
+	let out = new Uint8Array(length);
+
+	// Loop and convert.
+	while (length--) {
+		out[length] = bytes.charCodeAt(length);
+	}
+
+	return new Blob([out], { type: type });
+};
+
+/**
+ * Standardize Choices
+ *
+ * @param {null|!Playlist|!Array<WeightedChoice>} choices Choices.
+ * @return {null|!Array<WeightedChoice>} Choices.
+ */
+export const standardizeChoices = function(choices) {
+	if (null === choices) {
+		return null;
+	}
+	else if ('number' === typeof choices) {
+		return [[choices, 1]];
+	}
+
+	return choices;
+};
+
+/**
  * Zero Pad
  *
  * Pad a number with leading zeroes if needed.
@@ -72,23 +121,6 @@ export const zeroPad = function(v, length) {
 	}
 
 	return str;
-};
-
-/**
- * Standardize Choices
- *
- * @param {null|!Playlist|!Array<WeightedChoice>} choices Choices.
- * @return {null|!Array<WeightedChoice>} Choices.
- */
-export const standardizeChoices = function(choices) {
-	if (null === choices) {
-		return null;
-	}
-	else if ('number' === typeof choices) {
-		return [[choices, 1]];
-	}
-
-	return choices;
 };
 
 
