2eab9ddc3df45e965909ad68d326b5618574ce5b
Josh Stoik <josh@blobfolio.com>
2019-09-29 19:35:12 -0700

    fix: move base64toBlob to _bin.mjs to force correct call order

--- src/js/_bin.mjs
+++ src/js/_bin.mjs
@@ -2,9 +2,37 @@
  * @file Binary data.
  */
 
-import { base64toBlob } from './_helpers.mjs';
+/**
+ * Base64 to Blob
+ *
+ * It's weird. Binary data stored as base64-encoded strings takes up
+ * much less disk space than the same data stored in a TypedArray, but
+ * during runtime, data-URIs actually require more memory.
+ *
+ * Because our file values are constants, we can enjoy the best of both
+ * worlds by programmatically converting base64 to Blob once at startup.
+ *
+ * @param {string} data Data.
+ * @param {string} type Content type.
+ * @return {!Blob} Blob.
+ */
+export const base64toBlob = function(data, type) {
+	/** @const {string} */
+	const bytes = atob(data);
 
+	/** @type {number} */
+	let length = bytes.length;
 
+	/** @type {Uint8Array} */
+	let out = new Uint8Array(length);
+
+	// Loop and convert.
+	while (length--) {
+		out[length] = bytes.charCodeAt(length);
+	}
+
+	return new Blob([out], { type: type });
+};
 
 /**
  * Sprite Image
