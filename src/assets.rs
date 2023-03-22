b35e27df0ac8a6d995065c5b88bfebddb663db2d
Josh Stoik <josh@blobfolio.com>
2023-03-21 17:52:00 -0700

    cleanup: don't need to keep the blobs

--- src/assets.rs
+++ src/assets.rs
@@ -15,26 +15,17 @@ use web_sys::{
 	Url,
 };
 
-
+// Register the Blob/URLs _once_ and hold onto them since garbage collection is
+// iffy.
 thread_local! {
-	// Create and hold onto a Blob representation of the image data to keep it
-	// in memory for the lifetime of the program.
-	static IMG_POE: Blob = slice_to_blob(include_bytes!("../skel/img/poe.png"), "image/png");
-
-	// Generate and hold onto a URL to keep the browser from repeating itself.
-	static IMG_POE_URL: String = IMG_POE.with(Url::create_object_url_with_blob).unwrap_throw();
+	static IMG_POE: String = url(include_bytes!("../skel/img/poe.png"), "image/png");
 }
 
 #[cfg(not(feature = "firefox"))]
 thread_local! {
-	// Same as with the image; make and keep some Blobs and URLs.
-	static SND_BAA: Blob = slice_to_blob(include_bytes!("../skel/sound/baa.flac"), "audio/flac");
-	static SND_SNEEZE: Blob = slice_to_blob(include_bytes!("../skel/sound/sneeze.flac"), "audio/flac");
-	static SND_YAWN: Blob = slice_to_blob(include_bytes!("../skel/sound/yawn.flac"), "audio/flac");
-
-	static SND_BAA_URL: String = SND_BAA.with(Url::create_object_url_with_blob).unwrap_throw();
-	static SND_SNEEZE_URL: String = SND_SNEEZE.with(Url::create_object_url_with_blob).unwrap_throw();
-	static SND_YAWN_URL: String = SND_YAWN.with(Url::create_object_url_with_blob).unwrap_throw();
+	static SND_BAA: String = url(include_bytes!("../skel/sound/baa.flac"), "audio/flac");
+	static SND_SNEEZE: String = url(include_bytes!("../skel/sound/sneeze.flac"), "audio/flac");
+	static SND_YAWN: String = url(include_bytes!("../skel/sound/yawn.flac"), "audio/flac");
 }
 
 
@@ -47,7 +38,7 @@ pub(crate) fn sprite_image_element() -> HtmlImageElement {
 	let el = HtmlImageElement::new_with_width_and_height(Frame::SPRITE_WIDTH, Frame::SPRITE_HEIGHT)
 		.unwrap_throw();
 	el.set_id("i");
-	IMG_POE_URL.with(|s| el.set_src(s));
+	IMG_POE.with(|s| el.set_src(s));
 	el
 }
 
@@ -69,9 +60,9 @@ impl Sound {
 		use web_sys::HtmlAudioElement;
 
 		match self {
-			Self::Baa => SND_BAA_URL.with(|s| HtmlAudioElement::new_with_src(s).ok()),
-			Self::Sneeze => SND_SNEEZE_URL.with(|s| HtmlAudioElement::new_with_src(s).ok()),
-			Self::Yawn => SND_YAWN_URL.with(|s| HtmlAudioElement::new_with_src(s).ok()),
+			Self::Baa => SND_BAA.with(|s| HtmlAudioElement::new_with_src(s).ok()),
+			Self::Sneeze => SND_SNEEZE.with(|s| HtmlAudioElement::new_with_src(s).ok()),
+			Self::Yawn => SND_YAWN.with(|s| HtmlAudioElement::new_with_src(s).ok()),
 		}
 			.and_then(|obj| obj.play().ok());
 	}
@@ -79,10 +70,12 @@ impl Sound {
 
 
 
-/// # Slice to Blob.
-fn slice_to_blob(data: &'static [u8], mime: &str) -> Blob {
+/// # Slice to Blob to URL.
+fn url(data: &'static [u8], mime: &str) -> String {
 	Blob::new_with_u8_array_sequence_and_options(
 		&Array::of1(&Uint8Array::from(data)),
 		BlobPropertyBag::new().type_(mime)
-	).unwrap_throw()
+	)
+		.and_then(|b| Url::create_object_url_with_blob(&b))
+		.unwrap_throw()
 }
